e420ebb47cabf0132115dba99e15c24880bb08dc
Dwight Hubbard <d@dhub.me>
2015-03-14 09:09:29 -0700

    Cleanups

---
+++ tests/test_unit.py
@@ -0,0 +1,22 @@
+# Copyright (c) 2015, Dwight Hubbard
+# Copyrights licensed under the Apache 2.0 License
+# See the accompanying LICENSE.txt file for terms.
+"""
+----------------------------------
+
+Tests for `redislite` module.
+"""
+from __future__ import print_function
+import logging
+import unittest
+
+
+# noinspection PyPep8Naming
+class TestConfset(unittest.TestCase):
+    def setUp(self):
+        pass
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    unittest.main()
