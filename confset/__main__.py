cf6c9238a1da4f8559e375096711725761a88be3
Dwight Hubbard <254983+dwighthubbard@users.noreply.github.com>
2020-01-06 16:28:44 -0800

    Update __main__.py

--- confset/__main__.py
+++ confset/__main__.py
@@ -1,18 +1,4 @@
-import sys
-from .arguments import ConfsetArguments, create_arg_parser
-
-
-def main():
-    parser = create_arg_parser()
-    (options, args) = parser.parse_args()
-    confset = ConfsetArguments(args, options)
-
-    try:
-        confset.execute()
-    except FileNotFoundError:
-        print('No writable configuration directories found', file=sys.stderr)
-        return 1
-    return 0
+from .cli import main
 
 
 if __name__ == '__main__':
